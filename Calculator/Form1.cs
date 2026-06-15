06af91a646a3c1f66f20f24a227c32962554e837
Verzecht <thetonyg999@gmail.com>
2026-06-15 11:47:07 -0500

    Update Form1.cs
    removed unused code

--- Calculator/Form1.cs
+++ Calculator/Form1.cs
@@ -23,11 +23,6 @@ namespace Calculator
             InitializeComponent();
         }
 
-        private void Form1_Load(object sender, EventArgs e)
-        {
-        
-        }
-
         private void DigitButton_Click(object sender, EventArgs e)
         {
 
